388019cf6071d075ee831a2701bee0429dafbb9f
Istaufiqu <taufiqmail01@gmail.com>
2024-11-14 10:26:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function showMenu(menuId) {
     const selectedMenu = document.getElementById(menuId);
     selectedMenu.style.display = 'block';
 }
+
+// Fungsi untuk membuka dan menutup menu
+function toggleMenu() {
+    var menu1 = document.getElementById("menu1-nav");
+    var menu2 = document.getElementById("menu2-nav");
+    var openMenuBtn = document.getElementById("openMenuBtn");
+
+    // Menampilkan menu dan menyembunyikan tombol
+    if (menu1.style.display === "block" || menu2.style.display === "block") {
+        menu1.style.display = "none";
+        menu2.style.display = "none";
+    } else {
+        menu1.style.display = "block";
+        menu2.style.display = "block";
+        openMenuBtn.style.display = "none"; // Menyembunyikan tombol "Buka Menu"
+    }
+}
